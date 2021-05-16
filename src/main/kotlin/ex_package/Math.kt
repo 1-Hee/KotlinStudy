d097458dd0c7c45760d36cfd219da59c0102035e
JOWONHEE <79094527+1-Hee@users.noreply.github.com>
2021-05-16 22:17:54 +0900

    Add files via upload

---
+++ src/main/kotlin/ex_package/Math.kt
@@ -0,0 +1,14 @@
+package ex_package
+
+// a와 b중 더 큰 수를 반환하는 함수
+fun max(aG:Int, bG:Int):Int =
+    if (aG>bG) aG else bG
+
+// a와 b 중 더 작은 수를 반환하는 함수
+fun min(aG:Int, bG:Int):Int =
+    if (aG<bG) aG else bG
+
+// num의 절대값을 반환하는 함수
+fun abs(numG:Int):Int =
+    if (numG >= 0 ) numG else -numG
+
