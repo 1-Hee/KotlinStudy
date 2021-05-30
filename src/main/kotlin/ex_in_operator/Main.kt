b450c0be16414f937a64d40571435c9703d9ad4e
JOWONHEE <79094527+1-Hee@users.noreply.github.com>
2021-05-30 17:51:56 +0900

    Add files via upload

---
+++ src/main/kotlin/ex_in_operator/Main.kt
@@ -0,0 +1,10 @@
+package ex_in_operator
+
+fun main(args:Array<String>){
+
+    //chap 62. In 연산자
+
+    println('o' in "kotiln") // kotlin이라는 문자 안에 o가 있는지에 대한 논리 연산자
+    println("in" !in "Kotiln") // Kotlin이라는 문자 안에 in이 없는지에 대한 논리 연산자
+
+}
